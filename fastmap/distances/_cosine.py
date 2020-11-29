62039c618fbc434940dff341f53b26f34245adee
Shawn Davis <>
2020-11-28 21:19:17 -0500

    Coding up the API's for distance metrics. Jaccard distance is implemented. Working on a cosine similarity based distance.

--- fastmap/distances/_cosine.py
+++ fastmap/distances/_cosine.py
@@ -2,16 +2,24 @@ from fastmap import Distance, InputError
 from math import sqrt
 from pandas.api.types import is_list_like
 
+
 def _to_dict(x):
     if isinstance(x, dict):
         return x
     else:
         return {idx: count for (idx, count) in enumerate(x) if count != 0}
 
+
 def _match_inputs(x, y):
     if type(x) == type(y):
         return x, y
     elif isinstance(x, dict) or isinstance(y, dict):
+        d, coll = (x, y) if isinstance(x, dict) else (y, x)
+        return (d, _to_dict(coll))
+    else:
+        assert len(x) == len(y), "Non-set, non-dict list-like inputs must have the same length"
+        return x, y
+
 
 class Cosine(Distance):
 
